50a5bad28e37fbd5920ec32258f0e5a376b3e1af
Ross Johnstone <rhjohnstone+gitlab@pm.me>
2020-05-05 16:14:20 +0900

    Add model 6, hierarchical with varying saturation

--- johnstone.py
+++ johnstone.py
@@ -33,12 +33,14 @@ sat_lower = 0.
 sat_upper = 2.
 
 # Hyperparameters for hierarchical model
+# We use experimental results from https://doi.org/10.1016/j.vascn.2013.04.007,
+# along with some empirically-chosen values, to inform our prior distributions
 elkins_pic50_mus = [5.235, 5.765, 6.060, 5.315, 5.571, 7.378, 7.248, 5.249, 
                     6.408, 5.625, 7.321, 6.852, 6.169, 6.217, 5.927, 7.414,
                     4.860]
 mu_mode = np.mean(elkins_pic50_mus)
-mu_lower = -4  # diminishing returns on lowering pIC50
-mu_shape = 7.5  # empirical
+mu_lower = -4
+mu_shape = 7.5
 mu_rate = gamma_distn_rate(mu_shape, mu_mode, mu_lower)
 
 elkins_pic50_ss = [0.0760, 0.1388, 0.1459, 0.2044, 0.1597, 0.2216, 0.1856,
@@ -75,39 +77,48 @@ def expt_model(model_number, concs, responses, expt_labels):
     
     You can use numpy.arrays of data, but not numpy operations when
     transforming variables, etc., because PyMC3 uses Theano tensors.
+    
+    Because of how PyMC3 deals with transformation of variables, I chose to use
+    pm.Deterministic, which stores both the original and transformed variable
+    samples. This is slightly annoying, as it doubles the memory used. For a
+    large number of parameters or sampling iterations, this could be a problem.
+    Given the relatively small number of parameters in the models we are
+    considering here, I don't think it is a problem. I keep a list of the
+    original parameters I don't need later, and remove them after sampling is
+    finished. There are some solutions that don't require storing both sets of
+    parameters, but they are more difficult to deal with after the sampling is
+    finished. If we start experimenting with models with many more parameters,
+    we may have to revisit these alternatives. But I believe the current
+    solution has the best balance for now.
     """
-
-
     n_expts = expt_labels.max() + 1
-
     with pm.Model() as model:
-
         
         # Noise standard deviation sigma, shared by all models.
         sigma = pm.Gamma("xsigma", alpha=sigma_shape, beta=sigma_rate)
         sigma_shift = pm.Deterministic(r"$\sigma$", sigma + sigma_lower)
     
-        if model_number == 1:
+        if model_number == 1:  # Single-level
             pic50 = pm.Exponential("xpIC50", lam=pic50_rate)
             pic50_shift = pm.Deterministic("pIC50", pic50 + pic50_lower)
             hill = 1
             sat = 0
             remove = ["xsigma", "xpIC50"]
-        elif model_number == 2:
+        elif model_number == 2:  # Single-level
             pic50 = pm.Exponential("xpIC50", lam=pic50_rate)
             pic50_shift = pm.Deterministic("pIC50", pic50 + pic50_lower)
             loghill = pm.Logistic("xlogHill", mu=loghill_mu, s=loghill_s)
             hill = pm.Deterministic("Hill", pm.math.exp(loghill))
             sat = 0
             remove = ["xsigma", "xpIC50", "xlogHill"]
-        elif model_number == 3:
+        elif model_number == 3:  # Single-level
             pic50 = pm.Exponential("xpIC50", lam=pic50_rate)
             pic50_shift = pm.Deterministic("pIC50", pic50 + pic50_lower)
             loghill = pm.Logistic("xlogHill", mu=loghill_mu, s=loghill_s)
             hill = pm.Deterministic("Hill", pm.math.exp(loghill))
             sat = pm.Uniform("Saturation", lower=sat_lower, upper=sat_upper)
             remove = ["xsigma", "xpIC50", "xlogHill"]
-        elif model_number == 4:
+        elif model_number == 4:  # Hierarchical
             hill = 1
             sat = 0
             mu = pm.Gamma("xmu", alpha=mu_shape, beta=mu_rate)
@@ -117,7 +128,7 @@ def expt_model(model_number, concs, responses, expt_labels):
             pic50 = pm.Logistic("pIC50", mu=mu_shift, s=s_shift, shape=n_expts)
             pic50_shift = pic50[expt_labels]
             remove = ["xsigma", "xmu", "xs"]
-        elif model_number == 5:
+        elif model_number == 5:  # Hierarchical
             sat = 0
             mu = pm.Gamma("xmu", alpha=mu_shape, beta=mu_rate)
             mu_shift = pm.Deterministic(r"$\mu$", mu + mu_lower)
@@ -133,7 +144,23 @@ def expt_model(model_number, concs, responses, expt_labels):
                                   shape=n_expts)
             xhill = pm.Deterministic("Hill", pm.math.exp(loghill))
             hill = xhill[expt_labels]
+            remove = ["xsigma", "xmu", "xs", "xbeta", "xlogHill"]
+        elif model_number == 6:  # Hierarchical
+            sat = pm.Uniform("Saturation", lower=sat_lower, upper=sat_upper)
+            mu = pm.Gamma("xmu", alpha=mu_shape, beta=mu_rate)
+            mu_shift = pm.Deterministic(r"$\mu$", mu + mu_lower)
+            s = pm.Gamma("xs", alpha=s_shape, beta=s_rate)
+            s_shift = pm.Deterministic("s", s + s_lower)
+            pic50 = pm.Logistic("pIC50", mu=mu_shift, s=s_shift, shape=n_expts)
+            pic50_shift = pic50[expt_labels]
             
+            alpha = pm.Gamma(r"$\alpha$", alpha=alpha_shape, beta=alpha_rate)
+            beta = pm.Gamma("xbeta", alpha=beta_shape, beta=beta_rate)
+            beta_shift = pm.Deterministic(r"$\beta$", beta + beta_lower)
+            loghill = pm.Logistic("xlogHill", mu=alpha, s=beta_shift,
+                                  shape=n_expts)
+            xhill = pm.Deterministic("Hill", pm.math.exp(loghill))
+            hill = xhill[expt_labels]
             remove = ["xsigma", "xmu", "xs", "xbeta", "xlogHill"]
         
         # Data likelihood, shared by all models.
